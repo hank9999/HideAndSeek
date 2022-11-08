7aefd90c7ddb6af4b262ac9a7ba30ad1b3c9ddb6
hank9999 <hank9999@qq.com>
2022-11-08 14:07:06 +0800

    Style: Remove explicitly typed arguments

--- src/main/java/org/EncryptSL/blockhunt/W.java
+++ src/main/java/org/EncryptSL/blockhunt/W.java
@@ -28,8 +28,8 @@ public class W {
 	/*
 	 * Standard stuff.
 	 */
-	public static ArrayList<String> newFiles = new ArrayList<String>();
-	public static ArrayList<CommandM> commands = new ArrayList<CommandM>();
+	public static ArrayList<String> newFiles = new ArrayList<>();
+	public static ArrayList<CommandM> commands = new ArrayList<>();
 
 	/*
 	 * If you want another file to be created. Copy and paste this line.
@@ -44,19 +44,19 @@ public class W {
 	 * Add any variable you need in different classes here:
 	 */
 
-	public static HashMap<Player, LocationSerializable> pos1 = new HashMap<Player, LocationSerializable>();
-	public static HashMap<Player, LocationSerializable> pos2 = new HashMap<Player, LocationSerializable>();
+	public static HashMap<Player, LocationSerializable> pos1 = new HashMap<>();
+	public static HashMap<Player, LocationSerializable> pos2 = new HashMap<>();
 
-	public static ArrayList<Arena> arenaList = new ArrayList<Arena>();
+	public static ArrayList<Arena> arenaList = new ArrayList<>();
 	public static Random random = new Random();
-	public static HashMap<Player, Integer> seekertime = new HashMap<Player, Integer>();
+	public static HashMap<Player, Integer> seekertime = new HashMap<>();
 
-	public static HashMap<Player, PlayerArenaData> pData = new HashMap<Player, PlayerArenaData>();
-	public static HashMap<Player, ItemStack> choosenBlock = new HashMap<Player, ItemStack>();
-	public static HashMap<Player, Boolean> choosenSeeker = new HashMap<Player, Boolean>();
+	public static HashMap<Player, PlayerArenaData> pData = new HashMap<>();
+	public static HashMap<Player, ItemStack> choosenBlock = new HashMap<>();
+	public static HashMap<Player, Boolean> choosenSeeker = new HashMap<>();
 
-	public static HashMap<Player, ItemStack> pBlock = new HashMap<Player, ItemStack>();
-	public static HashMap<Player, Location> moveLoc = new HashMap<Player, Location>();
-	public static HashMap<Player, Location> hiddenLoc = new HashMap<Player, Location>();
-	public static HashMap<Player, Boolean> hiddenLocWater = new HashMap<Player, Boolean>();
+	public static HashMap<Player, ItemStack> pBlock = new HashMap<>();
+	public static HashMap<Player, Location> moveLoc = new HashMap<>();
+	public static HashMap<Player, Location> hiddenLoc = new HashMap<>();
+	public static HashMap<Player, Boolean> hiddenLocWater = new HashMap<>();
 }
