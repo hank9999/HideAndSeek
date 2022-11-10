17d562b50aac426f421ec8fc048003e9abf2a109
hank9999 <hank9999@qq.com>
2022-11-10 18:15:29 +0800

    Fix: Theoretical fix CMDwand.java (null safe)

--- src/main/java/org/EncryptSL/blockhunt/Commands/CMDwand.java
+++ src/main/java/org/EncryptSL/blockhunt/Commands/CMDwand.java
@@ -20,29 +20,33 @@ public class CMDwand extends DefaultCMD {
 	public boolean exectue(Player player, Command cmd, String label,
 			String[] args) {
 		if (player != null) {
-			ItemStack wand = new ItemStack(
-					Material.getMaterial((String) W.config
-							.get(ConfigC.wandIDname)));
-			ItemMeta im = wand.getItemMeta();
-			im.setDisplayName(MessageM.replaceAll((String) W.config
-					.get(ConfigC.wandName)));
-			W.config.load();
-			List<String> lores = W.config.getFile().getStringList(
-					ConfigC.wandDescription.location);
-			List<String> lores2 = new ArrayList<String>();
-			for (String lore : lores) {
-				lores2.add(MessageM.replaceAll(lore));
+			Material m = Material.getMaterial((String) W.config.get(ConfigC.wandIDname));
+			if (m == null) {
+				m = Material.STONE;
 			}
+			ItemStack wand = new ItemStack(m);
+			ItemMeta im = wand.getItemMeta();
+			if (im != null) {
+				im.setDisplayName(MessageM.replaceAll((String) W.config.get(ConfigC.wandName)));
+				W.config.load();
+				List<String> lores = W.config.getFile().getStringList(
+						ConfigC.wandDescription.location);
+				List<String> lores2 = new ArrayList<>();
+				for (String lore : lores) {
+					lores2.add(MessageM.replaceAll(lore));
+				}
 
-			im.setLore(lores2);
-			wand.setItemMeta(im);
-			player.getInventory().addItem(wand);
-			player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 5, 0);
-			MessageM.sendFMessage(player, ConfigC.normal_wandGaveWand, "type-"
-					+ wand.getType().toString().replaceAll("_", " ")
-							.toLowerCase());
+				im.setLore(lores2);
+				wand.setItemMeta(im);
+				player.getInventory().addItem(wand);
+				player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 5, 0);
+				MessageM.sendFMessage(
+						player,
+						ConfigC.normal_wandGaveWand,
+						"type-" + wand.getType().toString().replaceAll("_", " ").toLowerCase());
+			}
 		} else {
-			MessageM.sendFMessage(player, ConfigC.error_onlyIngame);
+			MessageM.sendFMessage(null, ConfigC.error_onlyIngame);
 		}
 		return true;
 	}
